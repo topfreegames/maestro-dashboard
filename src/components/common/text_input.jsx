185f21e77c03a4fc0877a4c9bd1bf5b58caa8258
Yuri Brito <yuriabrito@gmail.com>
2017-10-04 18:54:21 -0300

    TextInput focus bar animation

--- src/components/common/text_input.jsx
+++ src/components/common/text_input.jsx
@@ -12,6 +12,7 @@ const TextInput = ({ id, label, value, handleChange, error }) => (
       onChange={handleChange}
       className={error && 'error'}
     />
+    <div className='bar' />
     {error && error.map(e => <div key={e.msg}>{e.msg}</div>)}
   </div>
 )
@@ -26,16 +27,36 @@ TextInput.styles = css({
     color: styles.colors.gray_75
   },
 
+  '& .bar': {
+    position: 'relative',
+    display: 'block',
+    margin: 0,
+    padding: 0,
+    height: 0,
+
+    '&::before, &::after': {
+      content: `''`,
+      position: 'absolute',
+      bottom: 0,
+      backgroundColor: styles.colors.brandPrimary,
+      height: '2px',
+      width: 0,
+      transition: '0.2s ease all'
+    },
+
+    '&::before': { left: '50%' },
+    '&::after': { right: '50%' }
+  },
+
   '& input': {
+    position: 'relative',
     fontSize: styles.fontSizes['3'],
     padding: '6px 0 6px',
     border: 'none',
     borderBottom: `1px solid ${styles.colors.gray_50}`,
 
-    '&:focus': {
-      fontWeight: 500,
-      padding: '6px 0 5px',
-      borderBottom: `2px solid ${styles.colors.brandPrimary}`
+    '&:focus ~ .bar::before, &:focus ~ .bar::after': {
+      width: '50%'
     },
 
     '&.error': {
