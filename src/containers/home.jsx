16f3f390d7f6dbeafe1a89247adaed63868919a9
Yuri Brito <yuriabrito@gmail.com>
2017-10-04 16:01:14 -0300

    signin

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { css } from 'glamor'
+import GoogleButton from 'react-google-button'
 import { getCode } from 'actions/session'
+import { navigate } from 'actions/common'
+import styles from 'constants/styles'
 
-class Home extends React.Component {
-  render () {
-    return <div onClick={() => getCode()}>SIGN IN</div>
-  }
+const signIn = event => {
+  event.preventDefault()
+  getCode()
 }
 
-export default Home
+class Home extends React.Component {
+  componentWillMount = () => {
+    if (this.props.session.code || this.props.session.token) {
+      navigate('dashboard')
+    }
+  }
+
+  render = () => (
+    <div {...Home.styles}>
+      <div>M</div>
+      <GoogleButton
+        type='light'
+        onClick={signIn}
+      />
+    </div>
+  )
+}
+
+Home.styles = css({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  height: '100vh',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: styles.colors.brandPrimary,
+
+  '> div:nth-of-type(1)': {
+    fontSize: '300px',
+    fontWeight: 600,
+    color: 'white'
+  }
+})
+
+export default connect(state => ({ session: state.session }))(Home)
