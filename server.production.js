b938805ed6917f9680469e736cdc11cdbc7d2331
Yuri Brito <yuriabrito@gmail.com>
2017-10-11 14:56:17 -0300

    serving manifest and static files

--- server.production.js
+++ server.production.js
@@ -1,8 +1,14 @@
+const express = require('express')
 const path = require('path')
 
 module.exports = app => {
+  app.use(express.static('build'))
+
   app.get('/bundle.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'build/bundle.js'))
+  })
+  app.get('/manifest.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/manifest.json'))
   })
 
   app.get('*', (req, res) => {
