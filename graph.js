9b3bdd8ebda6e1fee3fb67464aff3612aa04a4ec
Yuri Brito <yuriabrito@gmail.com>
2017-10-18 12:50:02 -0200

    graphs are embeds from datadog

--- graph.js
+++ graph.js
@@ -1,67 +1,51 @@
 const cache = require('memory-cache')
-const axios = require('axios')
+const request = require('request-promise')
 
-const credentials = () => {
-  const apiKey = process.env.DATADOG_API_KEY
-  const applicationKey = process.env.DATADOG_APPLICATION_KEY
-  return `api_key=${apiKey}&application_key=${applicationKey}`
-}
-
-const graphDef = (scheduler, region) => `%7B%22viz%22%3A%22timeseries%22%2C%22status%22%3A%22done%22%2C%22requests%22%3A%5B%7B%22q%22%3A%22max%3Amaestro.gru.ready%7Bmaestro-scheduler%3A${scheduler}%2Cmaestro-region%3A${region}%7D+%2B+max%3Amaestro.gru.occupied%7Bmaestro-scheduler%3A${scheduler}%2Cmaestro-region%3A${region}%7D%22%2C%22aggregator%22%3A%22avg%22%2C%22conditional_formats%22%3A%5B%5D%2C%22type%22%3A%22line%22%2C%22style%22%3A%7B%22palette%22%3A%22cool%22%7D%7D%2C%7B%22q%22%3A%22max%3Amaestro.gru.occupied%7Bmaestro-scheduler%3A${scheduler}%2Cmaestro-region%3A${region}%7D%22%2C%22style%22%3A%7B%22palette%22%3A%22warm%22%7D%2C%22type%22%3A%22line%22%7D%5D%2C%22autoscale%22%3Atrue%7D`
-
-const delay = time =>
-  new Promise(resolve => {
-    setTimeout(resolve, time)
-  })
-
-const maxInterval = 1000 * 60 * 5
-const pollingInterval = 1000 * 30
-const delayInterval = 1000 * 9
-
-const timeNow = () => (new Date()).getTime()
-const timeInSec = time => Math.round(time / 1000)
+const graphDef = (scheduler, region) =>
+  `{
+    "viz": "timeseries",
+    "status": "done",
+    "requests": [
+      {
+        "q": "max:maestro.gru.ready{maestro-scheduler:${scheduler},maestro-region:${region}} + max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}",
+        "aggregator": "avg",
+        "conditional_formats": [
+          
+        ],
+        "type": "line",
+        "style": {
+          "palette": "cool"
+        }
+      },
+      {
+        "q": "max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}",
+        "style": {
+          "palette": "warm"
+        },
+        "type": "line"
+      }
+    ],
+    "autoscale": true
+  }`
 
 const cacheKey = (scheduler, region) => `${scheduler}//${region}`
 
-const snapshotRequest = (scheduler, region) => {
-  const host = 'https://app.datadoghq.com/api/v1/graph/snapshot'
-  const end = timeNow()
-  const start = end - 60 * 60 * 1000
+const requestEmbed = (scheduler, region) => {
+  const host = 'https://app.datadoghq.com/api/v1/graph/embed'
 
-  const url =
-    `${host}?${credentials()}&start=${timeInSec(start)}&end=${timeInSec(end)}&graph_def=${graphDef(scheduler, region)}&title=Occupancy`
-
-  return axios.get(url)
-    .then(response => ({
-      snapshot: response.data.snapshot_url,
-      lastRequestAt: end
-    }))
-}
-
-const startPollingSnapshot = (scheduler, region) => {
-  const key = cacheKey(scheduler, region)
-  const cached = cache.get(key)
-
-  if (cached && (timeNow() - cached.lastRequestAt > maxInterval)) {
-    cache.del(key)
-    return
-  }
-
-  setTimeout(() => startPollingSnapshot(scheduler, region), pollingInterval)
-
-  return snapshotRequest(scheduler, region)
-    .then(({ snapshot, lastRequestAt }) => {
-      cache.put(key, {
-        snapshot,
-        lastRequestAt,
-        createdAt: lastRequestAt,
-        schedulerName: scheduler,
-        schedulerRegion: region
-      })
-
-      return snapshot
+  return request
+    .post(host)
+    .form({
+      api_key: process.env.DATADOG_API_KEY,
+      application_key: process.env.DATADOG_APPLICATION_KEY,
+      graph_json: graphDef(scheduler, region),
+      size: 'small',
+      timeframe: '1_hour',
+      title: 'Occupancy'
     })
-    .catch(err => console.log(err))
+    .then(response => ({
+      embedId: JSON.parse(response).embed_id
+    }))
 }
 
 const maybeRequest = (scheduler, region) => {
@@ -69,26 +53,17 @@ const maybeRequest = (scheduler, region) => {
   const cached = cache.get(key)
 
   if (cached) {
-    cache.put(key, {
-      ...cached,
-      lastRequestAt: timeNow()
-    })
-
-    if (timeNow() - cached.createdAt < delayInterval) {
-      return delay(delayInterval - (cached.lastRequestAt - cached.createdAt))
-        .then(() => cached.snapshot)
-    }
-
-    return Promise.resolve(cached.snapshot)
+    return Promise.resolve(cached)
   }
 
-  return startPollingSnapshot(scheduler, region).then(snapshot => {
-    return delay(delayInterval).then(() => snapshot)
+  return requestEmbed(scheduler, region).then(json => {
+    cache.put(key, json, 1000 * 60 * 60)
+    return json
   })
 }
 
 module.exports = app =>
   app.get('/graph', function (req, res, next) {
     const { scheduler, region } = req.query
-    maybeRequest(scheduler, region).then(snapshot => res.json({ snapshot }))
+    maybeRequest(scheduler, region).then(json => res.json(json))
   })
