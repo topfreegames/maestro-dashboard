910ac48e3ad80360df116c86aa175d2afcaab26c
Yuri Brito <yuriabrito@gmail.com>
2017-10-19 13:53:38 -0200

    shouldComponentUpdate for Graph

--- src/containers/schedulers/graph.jsx
+++ src/containers/schedulers/graph.jsx
@@ -39,12 +39,12 @@ class Graph extends React.Component {
     this.setState({ embedId })
   }
 
-  // shouldComponentUpdate = (nextProps, nextState) => (
-  //   ['region', 'upUsage', 'downUsage', 'timeframe']
-  //     .reduce((acc, e) => this.props[e] !== nextProps[e] || acc, false) ||
-  //   ['activeTimeframe', 'embedId']
-  //     .reduce((acc, e) => this.state[e] !== nextState[e] || acc, false)
-  // )
+  shouldComponentUpdate = (nextProps, nextState) => (
+    ['region', 'upUsage', 'downUsage', 'timeframe']
+      .reduce((acc, e) => this.props[e] !== nextProps[e] || acc, false) ||
+    ['activeTimeframe', 'embedId']
+      .reduce((acc, e) => this.state[e] !== nextState[e] || acc, false)
+  )
 
   componentDidMount = () => this.getEmbedId()
 
