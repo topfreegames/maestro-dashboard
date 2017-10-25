de0f3c5f4ab814d0fb32b9a93ae8920c87b4ca0b
Yuri Brito <yuriabrito@gmail.com>
2017-10-25 11:58:18 -0200

    datadog graphs rollup 30s

--- graph.js
+++ graph.js
@@ -7,7 +7,7 @@ const graphDef = ({ scheduler, region, upUsage, downUsage }) =>
     "status": "done",
     "requests": [
       {
-        "q": "max:maestro.gru.ready{maestro-scheduler:${scheduler},maestro-region:${region}} + max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}",
+        "q": "max:maestro.gru.ready{maestro-scheduler:${scheduler},maestro-region:${region}}.rollup(max, 30) + max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}.rollup(max, 30)",
         "aggregator": "avg",
         "conditional_formats": [],
         "type": "line",
@@ -16,7 +16,7 @@ const graphDef = ({ scheduler, region, upUsage, downUsage }) =>
         }
       },
       {
-        "q": "${upUsage / 100} * ( max:maestro.gru.ready{maestro-scheduler:${scheduler},maestro-region:${region}} + max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}} )",
+        "q": "${upUsage / 100} * ( max:maestro.gru.ready{maestro-scheduler:${scheduler},maestro-region:${region}}.rollup(max, 30) + max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}.rollup(max, 30) )",
         "aggregator": "avg",
         "conditional_formats": [],
         "type": "line",
@@ -27,7 +27,7 @@ const graphDef = ({ scheduler, region, upUsage, downUsage }) =>
         }
       },
       {
-        "q": "${downUsage / 100} * ( max:maestro.gru.ready{maestro-scheduler:${scheduler},maestro-region:${region}} + max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}} )",
+        "q": "${downUsage / 100} * ( max:maestro.gru.ready{maestro-scheduler:${scheduler},maestro-region:${region}}.rollup(max, 30) + max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}.rollup(max, 30) )",
         "aggregator": "avg",
         "conditional_formats": [],
         "type": "line",
@@ -38,7 +38,7 @@ const graphDef = ({ scheduler, region, upUsage, downUsage }) =>
         }
       },
       {
-        "q": "max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}",
+        "q": "max:maestro.gru.occupied{maestro-scheduler:${scheduler},maestro-region:${region}}.rollup(max, 30)",
         "style": {
           "palette": "warm"
         },
